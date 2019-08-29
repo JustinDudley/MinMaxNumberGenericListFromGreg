9fa3bcb1787f99aa4005ca37c2919b12b6116ddb
Greg Doud <gpdoud@gmail.com>
2019-08-29 16:05:52 -0400

    Add project files.

---
+++ MinMaxNumberProjectWithList/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MinMaxNumberProjectWithList {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
